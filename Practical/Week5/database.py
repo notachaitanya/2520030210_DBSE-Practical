a7f224c63c259254f5daf59034d3f192dcc5a506
notachaitanya <chaitanyakondapalli13542@gmail.com>
2026-09-23 21:49:46 +0530

    .

---
+++ Practical/Week5/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "postgresql+asyncpg://postgres:Iamchaitanya@13542@localhost:5432/bookflow_db"
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+AsyncSessionLocal = sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False
+)
+
+Base = declarative_base()
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
